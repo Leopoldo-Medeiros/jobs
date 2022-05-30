07857e57820c8b3294d1526d6d405c3001317694
leopoldomedeiros <leopoldof.medeiros@gmail.com>
2022-05-30 12:35:47 +0100

    Alpine.js & Store Data

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,9 @@
 @if(session()->has('message'))
-    <div class="fixed top-0 left-1/2 transform-translate-x-1/2 bg-laravel text-white px-48 py-3">
+    {{-- x-data: Basically when this is initialized, this message will disappear after a certain amount of time --}}
+    {{-- Attribute x-init: When it initializes  we use setTimeout() which takes a function --}}
+    {{-- Then it takes in a second parameter which is going to be the number of milliseconds where we want to wait until it goes way--}}
+    <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"
+         class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
         <p>
             {{session('message')}}
         </p>
